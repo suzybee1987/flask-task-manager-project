f02879787e9ebb8dd09a0543a9e2612f5cfda150
suzybee1987 <suzannemiller1987@gmail.com>
2021-09-22 11:08:38 +0000

    DEv: create add_task template

--- app.py
+++ app.py
@@ -96,6 +96,11 @@ def logout():
     flash("You have been logged out!")
     session.pop("user")
     return redirect(url_for("login"))
+
+
+@app.route("/add_task")
+def add_task():
+    return render_template("add_task.html")
         
 
 
